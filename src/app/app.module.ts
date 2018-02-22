c187003724bcdf040c2eef4b791b8316af030f49
paul sanchez <paulpsan@gmail.com>
2018-02-21 20:36:38 -0400

    cambio de directorio de  material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '../material/material.module';
+import { MaterialModule } from './material/material.module';
 
 
 import { AppComponent } from './app.component';
